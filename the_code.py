11117022359dabc87f9521eb64beb0cda179fb54
CasePaper33831 <87284113+CasePaper33831@users.noreply.github.com>
2023-09-13 15:29:52 -0500

    Update the_code.py

--- the_code.py
+++ the_code.py
@@ -1,32 +1,48 @@
 '''TODO:
 DONE: 
--minnimum range of passcode length is TWELVE (12) 
+-minimum range of passcode length is TWELVE (12) 
 -lower and upper case letter(s) forced output
 -digit(s) (0 through 9) forced output
 -special character(s) forced output
-
-IN PROGRESS: 
 -ask user for how many versions of passcode
 -put text on popup if possible
+
+IN PROGRESS: 
+none
 
 Everyone allowed to use code with no worries about asking permissions
 '''
 import string
 import random
+import tkinter as tk
+from tkinter import messagebox
 
 characters = string.digits + string.ascii_lowercase + string.ascii_uppercase + "!@#$%^&*()_+-=[]{;:,}.<>/?"
 
 def passcode_length():
     return int(input("Please enter passcode length in numbers with no commas (minimum value 12): "))
 
-while True:
-    length = passcode_length()
-    if length < 12:
-        print("Invalid input. Please enter a number 12 or higher, with no commas.")
-        
+def passcode_iteration():
+    return int(input("Please input the number of passcode variants (minimum value 0): "))
+
+root = tk.Tk()
+root.withdraw()
+length = passcode_length()
+if length < 12:
+    messagebox.showerror("Invalid Input", "Invalid passcode length input. Please enter a number 12 or higher, with no commas.")
+else:
+    iterations = passcode_iteration()
+    if iterations <= 0:
+        messagebox.showerror("Invalid Input", "Invalid iteration amount input. Please enter a number 1 or higher, with no commas.")
     else:
-        passcode = ''
-        while not any(char in passcode for char in "!@#$%^&*()_{+-=[]};:,.<>/?"):
-            passcode = ''.join(random.choice(characters) for i in range(length))
-        print(str("Your new passcode is: " + passcode))
-        break
+        passcodes = []
+        for i in range(iterations):
+            passcode = ''
+            while not any(char in passcode for char in "!@#$%^&*()_{+-=[]};:,.<>/?"):
+                passcode = ''.join(random.choice(characters) for _ in range(length))
+            passcodes.append(passcode)
+
+        passcode_message = "\n".join(f"Passcode {i + 1}: {passcode}" for i, passcode in enumerate(passcodes))
+        messagebox.showinfo("Passcodes", passcode_message)
+
+root.destroy()
